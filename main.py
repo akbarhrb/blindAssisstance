0336894a4daaf4ec024aea74d0d0201fce27be50
Ali Harb <133803169+akbarhrb@users.noreply.github.com>
2025-04-07 12:09:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,23 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Replace with your actual class list
-CLASS_NAMES = ['person', 'bicycle', 'car', 'hand']  # etc.
+CLASS_NAMES = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+    'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+    'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+    'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+    'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+    'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+    'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+    'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+    'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+    'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
 
 def preprocess(image: Image.Image):
     image = image.resize((640, 640)).convert("RGB")
